a46f735bf10a381aaa35612b3d6189be65e4f36e
Peter Mallett <peter.mallett@gmail.com>
2011-07-18 21:24:49 -0400

    Add candy splitting code.

--- 2011/quals/3-candy-splitting/candy-splitting.py
+++ 2011/quals/3-candy-splitting/candy-splitting.py
@@ -1,6 +1,16 @@
-#sum = 3 ^ 5 ^ 6
-#print(sum)
-#exit()
+def split_candy(input_string):
+	weights = [int(n) for n in input_string.split(' ')]
+	patrick_sum = weights[0]
+	for n in weights[1:]:
+		patrick_sum = patrick_sum ^ n
+	if patrick_sum == 0:
+		weights.sort()
+		sean_sum = 0
+		for n in weights[1:]:
+			sean_sum += n
+		return sean_sum	
+	else:
+		return False
 
 ##
  # Accepts program input as a number of commands to follow, follewed by strings of input
@@ -27,4 +37,9 @@ case_number = 1
 for i, input_string in enumerate(inputs):
 	# each odd input line is the list of weight values
 	if i % 2 == 1:
-		print(input_string)
+		result = split_candy(input_string)
+		if result == False:
+			result = 'NO'
+		print('Case #{0}: {1}'.format(case_number, result))
+		case_number += 1
+
