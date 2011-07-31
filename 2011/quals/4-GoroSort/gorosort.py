216b9d52aca1f7993f1d7c1f8a2298dfd6feceb5
Peter Mallett <peter.mallett@gmail.com>
2011-07-30 20:05:25 -0400

    Add solution to GoroSort problem.

--- 2011/quals/4-GoroSort/gorosort.py
+++ 2011/quals/4-GoroSort/gorosort.py
@@ -3,13 +3,21 @@ sys.path.append('../../')
 import codejam
 
 def goro_sort(list):
-	
+	count_unsorted = 0
+	for i, v in enumerate(list):
+		if (i + 1 == int(v)):
+			count_unsorted += 1
+	return (len(list) - count_unsorted)
+		
 
 #accept two lines of input for each test case
 inputs = codejam.accept_input(2)
 
+case_number = 0
 for i, v in enumerate(inputs):
 	if (i % 2 != 0):
-		avg = goro_sort(v)
-		print('Case #{0}: {1}'.format(i, avg))
+		case_number += 1
+		input_list = v.split(' ') 
+		avg = goro_sort(input_list)
+		print('Case #{0}: {1:f}'.format(case_number, avg))
 
